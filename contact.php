462bca0bb2741ac016c0aac3782144495e8ccee2
Syed Faheemuddin <syedfaheemuddin@gmail.com>
2021-12-06 19:12:39 +0530

    first commit

---
+++ contact.php
@@ -0,0 +1,147 @@
+<?php
+
+include("header.php");
+
+
+
+?>
+
+    
+
+    <div id="templatemo_middle_subpage">
+
+    	<h2>Contact Us</h2>
+
+        <p>This is Syed (<span style="color:yellow">Hyderabad </span>). Did you know more about me <a target="_blank" href="http://www.google.com/">Click here</a></p>
+
+  </div>
+
+        
+
+    <div id="templatemo_main">
+
+
+
+        <div class="col_w900 col_w900_last">
+
+        
+
+            <div class="col_w420 float_l">
+
+            
+
+           	  <h3>Contact Form</h3>
+
+              
+
+              	<div id="cp_contact_form">
+
+                    
+
+                    <form method="post" name="contact" action="#">
+
+                    
+
+                            <label for="author">Name:</label> <input name="author" type="text" class="input_field" id="author" maxlength="60" />
+
+                          	<div class="cleaner_h10"></div>
+
+                            
+
+                            <label for="email">Email:</label> <input name="email" type="text" class="input_field" id="email" maxlength="60" />
+
+                          	<div class="cleaner_h10"></div>
+
+                            
+
+                            <label for="subject">Subject:</label> <input name="subject" type="text" class="input_field" id="subject" maxlength="60" />
+
+                            <div class="cleaner_h10"></div>
+
+
+
+                            <label for="text">Message:</label> <textarea id="text" name="text" rows="0" cols="0" class="required"></textarea>
+
+                            <div class="cleaner_h10"></div>
+
+                            
+
+                            <input type="submit" class="submit_btn float_l" name="submit" id="submit" value="Send" />
+
+                            <input type="reset" class="submit_btn float_r" name="reset" id="reset" value="Reset" />
+
+                        
+
+                   </form>
+
+    
+
+                </div>
+
+               
+
+            </div>
+
+            
+
+            <div class="col_w420 float_r" id="map">
+
+            
+
+            	<h3>Map</h3>
+
+                
+
+                <a href="images/map_big.jpg" title="location map">
+
+                    <img src="images/map_thumb.jpg" alt="location map" />
+
+                </a>
+
+
+
+				<div class="cleaner h30"></div>
+
+                                
+
+                <h3>Our Address</h3>
+
+                
+
+                   	  	Vetrivel pandian, North Street, Anna Nagar, <br />
+
+                        Udangudi, 628203<br />
+
+						Thoothukudi<br />
+
+                		<br />
+
+                     	Tel: 98-76-56-32-10 (Fake no)<br />
+
+                        Mail: jjvetri2@gmail.com (Original)
+
+            </div>
+
+            
+
+            <div class="cleaner"></div>
+
+		</div>
+
+        
+
+        <div class="cleaner"></div>
+
+    </div> <!-- end of main -->
+
+        
+
+</div> <!-- end of wrapper -->
+
+
+
+<?php
+
+include("footer.php");
+
+?>
